3bbbf4920cc1cc0f2a68acd2428acb271299728d
crubba <christian.rubba@googlemail.com>
2014-11-03 14:04:55 +0100

    added support for unbalanced/misspecified tables

--- R/span_header.R
+++ R/span_header.R
@@ -5,44 +5,42 @@ span_header <- function(header.names, header.colspans, header.rowspans, headerSe
   header.name.table <- vector()
 
   col <- 1
-  n.row <- length(header.names) #How many rows has the table? Maybe wrong under some circumstances
+  n.row <- length(header.names) #How many rows has the table? Maybe wrong under some circumstances when last row has rowspans > 2
 
-  repeat{ #column loop, change to while??
+  while(length(header.names[[2]]) == 0){#tests if misspecified table rows
 
-    row <- 1 #kann weg, ersetze row durch 1
-
-    if(header.rowspans[[row]][1] == n.row){#Does the cell span entire column?
+    if(header.rowspans[[1]][1] == n.row){#Does the cell span entire column?
 
       #create the header cell name
-      header.name.table <- append(header.name.table, header.names[[row]][1], length(header.name.table))
+      header.name.table <- append(header.name.table, header.names[[1]][1], length(header.name.table))
 
       #expand along columnes
-      length.col <- header.colspans[[row]][1]
-      header.colspans[[row]] <- append(header.colspans[[row]], rep(1, length.col - 1), 1)
-      header.rowspans[[row]] <- append(header.rowspans[[row]], rep(header.rowspans[[row]][1], length.col - 1), 1)
-      add.cols <- rep(header.names[[row]][1], length.col - 1) #paste(header.names[[row]][1], 1:length.col - 1, sep = ".")
-      header.names[[row]] <- append(header.names[[row]], add.cols, 1)
+      length.col <- header.colspans[[1]][1]
+      header.colspans[[1]] <- append(header.colspans[[1]], rep(1, length.col - 1), 1)
+      header.rowspans[[1]] <- append(header.rowspans[[1]], rep(header.rowspans[[1]][1], length.col - 1), 1)
+      add.cols <- rep(header.names[[1]][1], length.col - 1) #paste(header.names[[1]][1], 1:length.col - 1, sep = ".")
+      header.names[[1]] <- append(header.names[[1]], add.cols, 1)
 
       #remove the just introduced cell infos
-      header.colspans[[row]] <- header.colspans[[row]][-1] #check for colspans different
-      header.rowspans[[row]] <- header.rowspans[[row]][-1] #check for colspans different
-      header.names[[row]] <- header.names[[row]][-1]
+      header.colspans[[1]] <- header.colspans[[1]][-1] #check for colspans different
+      header.rowspans[[1]] <- header.rowspans[[1]][-1] #check for colspans different
+      header.names[[1]] <- header.names[[1]][-1]
 
     } else{
 
       #Take a look at colspans
-      #times <- header.colspans[[row]][1]
+      #times <- header.colspans[[1]][1]
       name <- header.names[[1]][1]
       keep.on <- TRUE
 
-      for(level in 1:n.row) {
+      for(level in 1:n.row) {#start at 1, correct?
 
         #expand along columnes
         length.col <- header.colspans[[level]][1]
         length.row <- header.rowspans[[level]][1]
 
         header.colspans[[level]] <- append(header.colspans[[level]], rep(1, length.col - 1), 1)
-        header.rowspans[[level]] <- append(header.rowspans[[level]], rep(header.rowspans[[row]][1], length.col - 1), 1) # replace header.rowspans[[row]][1] by 1
+        header.rowspans[[level]] <- append(header.rowspans[[level]], rep(header.rowspans[[1]][1], length.col - 1), 1) # replace header.rowspans[[row]][1] by 1
         header.names[[level]] <- append(header.names[[level]], rep(header.names[[level]][1], length.col - 1), 1)
 
         if(level > 1) {name <- paste(name, header.names[[level]][1], sep = headerSep)}
