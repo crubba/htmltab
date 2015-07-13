be59a4702f0ba8c8b591b0023b793588a430932c
crubba <christian.rubba@googlemail.com>
2015-07-13 21:32:16 +0200

    rm test_warnings.R

--- tests/testthat/test_warnings.R
+++
@@ -1,8 +0,0 @@
-context("Correct warning")
-
-test_that("Correctly expanded", {
-
-  url <- "http://www.tradingeconomics.com/netherlands/government-debt-to-gdp"
-  expect_that(htmltab(doc = url), gives_warning("Argument 'which' left unspecified. Choosing first table."))
-})
-
